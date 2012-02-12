3d543e377be4ebadf1c54d5f2352b5adb00db4cd
yuzuemon <killerqueen64@gmail.com>
2012-02-13 03:01:01 +0900

    fix neobundle setting

--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@ DOT_FILES=( .bazaar .gitconfig .gitignore_global .gvimrc .vim .vimrc .tmux.conf 
 
 for file in ${DOT_FILES[@]}
 do
+  if [ ! -L $1 ]; then
     ln -s $HOME/dotfiles/$file $HOME/$file
+  else
+    echo "既にシンボリックリンク済みです: $file"
+  fi
 done
