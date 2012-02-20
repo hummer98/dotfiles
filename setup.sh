57ef6fe96ad6a7ef6fe570a7a0edda9a7e6bfebc
yuzuemon <killerqueen64@gmail.com>
2012-02-21 01:10:22 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ DOT_FILES=( .bazaar .gitconfig .gitignore_global .gvimrc .vim .vimrc .tmux.conf 
 for file in ${DOT_FILES[@]}
 do
   if [ -a $HOME/$file ]; then
-    if [ -d $HOME/$file ]; then
-      echo "既にディレクトリが存在します: $file"
-    elif [ -s $HOME/$file ]; then
+    if [ -L $HOME/$file ]; then
       echo "既にシンボリックリンクが存在します: $file"
+    elif [ -d $HOME/$file ]; then
+      echo "既にディレクトリが存在します: $file"
     else
       echo "既にファイルが存在します: $file"
     fi
