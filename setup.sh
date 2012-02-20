839fed7c8897739bd5b1f63764b1922654a7b669
yuzuemon <killerqueen64@gmail.com>
2012-02-21 01:08:52 +0900

    add git co

--- setup.sh
+++ setup.sh
@@ -5,7 +5,13 @@ DOT_FILES=( .bazaar .gitconfig .gitignore_global .gvimrc .vim .vimrc .tmux.conf 
 for file in ${DOT_FILES[@]}
 do
   if [ -a $HOME/$file ]; then
-    echo "既にファイルかディレクトリかシンボリックリンクが存在します: $file"
+    if [ -d $HOME/$file ]; then
+      echo "既にディレクトリが存在します: $file"
+    elif [ -s $HOME/$file ]; then
+      echo "既にシンボリックリンクが存在します: $file"
+    else
+      echo "既にファイルが存在します: $file"
+    fi
   else
     ln -s $HOME/dotfiles/$file $HOME/$file
     echo "シンボリックリンクを張りました: $file"
