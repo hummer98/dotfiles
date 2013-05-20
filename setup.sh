26638bdd5b4f4226f5d27227916a4423ee39d450
yuzuemon <killerqueen64@gmail.com>
2013-05-20 15:39:36 +0900

    add .tigrc in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOT_FILES=( .bazaar .gitconfig .gitignore_global .gvimrc .vim .vimrc .tmux.conf .zshrc )
+DOT_FILES=( .bazaar .gitconfig .gitignore_global .gvimrc .vim .vimrc .tmux.conf .zshrc .tigrc )
 
 for file in ${DOT_FILES[@]}
 do
